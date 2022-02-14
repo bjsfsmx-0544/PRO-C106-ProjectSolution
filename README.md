946dff6ea6e763b1288e4003fca23d88a8c2896f
MarioCodi <mario.cortes@byjusfutureschool.com>
2022-02-14 15:10:04 -0600

    Traducción 1.0
    Modificación de comentarios. Creación de README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# PROC106-V1-plantilla-proyecto
+Plantilla del alumno.  
+Detectar al peatón.  
+  
+### Texto en inglés: PRO-C105-Project-Images
--- Walkers.py
+++ Walkers.py
@@ -1,29 +1,29 @@
 import cv2
 import numpy as np
 
-# Create our body classifier
+# Crear nuestro clasificador de cuerpos
 body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
-# Initiate video capture for video file
+# Inicializar la captura de video para nuestro archivo de video
 cap = cv2.VideoCapture('walking.avi')
 
-# Loop once video is successfully loaded
+# COmenzar el bucle una vez que el video esté cargado exitosamente
 while True:
     
-    # Read first frame
+    # Leer el primer cuadro
     ret, frame = cap.read()
     #frame = cv2.resize(frame, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Pass frame to our body classifier
+    # Pasar el cuadro a nuestro clasificador de cuerpos
     bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
     
-    # Extract bounding boxes for any bodies identified
+    # Extraer las cajas envolventes para cualquier cuerpo identificado
     for (x,y,w,h) in bodies:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
-        cv2.imshow('Pedestrians', frame)
+        cv2.imshow('Peatones', frame)
 
-    if cv2.waitKey(1) == 32: #32 is the Space Key
+    if cv2.waitKey(1) == 32: #32 es la barra espaciadora
         break
 
 cap.release()
